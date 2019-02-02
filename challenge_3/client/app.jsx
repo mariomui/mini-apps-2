28b53339735363f6d9cdd26b9e8a2be5b67d8ab3
mariomui <yi.kan.mui@gmail.com>
2019-02-01 18:26:46 -0800

    Implement 2nd scoreboard feature

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Grid from './reactparts/Grid';
+import ScoreBoard from './reactparts/ScoreBoard'
 
-function App(props) {
-  return (
-    <div>
-      hello
-      <Grid />
-    </div>
-  );
+
+// let framesArr = [];
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bowlingFrames: []
+    }
+  }
+  submitFrame = (bowlingFrame) => {
+    const { bowlingFrames } = this.state;
+
+
+    bowlingFrames.concat(bowlingFrame);
+    this.setState({
+      bowlingFrames: bowlingFrames.concat(bowlingFrame)
+
+    })
+  }
+  render() {
+
+    return (
+      <div>
+        hello
+        <Grid submitFrame={this.submitFrame} />
+        <ScoreBoard bowlingFrames={this.state.bowlingFrames} />
+      </div >
+    );
+  }
 }
 
 export default App;
